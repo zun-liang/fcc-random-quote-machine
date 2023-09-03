ce84e2352cb394428e753271dcba7dfdadcadb55
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-09-04 05:04:47 +0800

    format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react"
-import Error from "./components/Error"
-import Loading from "./components/Loading"
-import styled from "styled-components"
-import { CommonContainer } from "./styles/styles"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter, faTumblr } from '@fortawesome/free-brands-svg-icons'
+import { faTumblr, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import Error from "./components/Error";
+import Loading from "./components/Loading";
+import { CommonContainer } from "./styles/styles";
 
 const Container = styled(CommonContainer)`
   gap: 0;
-  background-color: ${props => props.color};
-`
+  background-color: ${(props) => props.color};
+`;
 const QuoteBox = styled.div`
   width: 300px;
   border-radius: 3px;
@@ -30,51 +31,53 @@ const QuoteBox = styled.div`
     width: 550px;
     padding: 40px 50px;
   }
-`
+`;
 const QuoteLeftIcon = styled(FontAwesomeIcon)`
   width: 20px;
   height: 20px;
   margin-right: 10px;
-  color: ${props => props.color};
+  color: ${(props) => props.color};
   transition: color 0s ease-in-out;
   @media (min-width: 600px) {
     width: 30px;
     height: 30px;
   }
-`
+`;
 const Quote = styled.h1`
-  color: ${props => props.color};
+  color: ${(props) => props.color};
   text-align: center;
   font-size: 20px;
   font-weight: 500;
   @media (min-width: 600px) {
     font-size: 28px;
   }
-`
+`;
 const Author = styled.p`
-  color: ${props => props.color};
+  color: ${(props) => props.color};
   text-align: right;
-`
+`;
 const IconBox = styled.a`
   width: 30px;
   height: 30px;
-  background-color: ${props => props.color};
+  background-color: ${(props) => props.color};
   border-radius: 3px;
   display: flex;
   justify-content: center;
   align-items: center;
   margin-right: 5px;
-  &:link, :visited {
+  &:link,
+  :visited {
     opacity: 1;
   }
-  &:hover, :active {
+  &:hover,
+  :active {
     opacity: 0.8;
   }
   @media (min-width: 600px) {
     width: 40px;
     height: 40px;
   }
-`
+`;
 const Icon = styled(FontAwesomeIcon)`
   width: 15px;
   height: 15px;
@@ -83,7 +86,7 @@ const Icon = styled(FontAwesomeIcon)`
     width: 18px;
     height: 18px;
   }
-`
+`;
 const Button = styled.button`
   justify-self: right;
   border: none;
@@ -92,8 +95,8 @@ const Button = styled.button`
   height: 30px;
   font-size: 12px;
   color: white;
-  background-color: ${props => props.color};
-  font-family: 'Raleway', sans-serif;
+  background-color: ${(props) => props.color};
+  font-family: "Raleway", sans-serif;
   font-weight: 500;
   display: flex;
   justify-content: center;
@@ -107,72 +110,100 @@ const Button = styled.button`
     height: 40px;
     font-size: 14px;
   }
-`
+`;
+
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const [quote, setQuote] = useState({quote: "", author: ""})
-  const [getNewQuote, setGetNewQuote] = useState(0)
-  const tweetContent = '"' + quote.quote + '" -' + quote.author
-  const colors = ["#2c3e50", "#342224", "#16a085", "#77b1a9", "#e74c3c", "#f39c12", "#73a857", "#bdbb99", "#472e32", "#27ae60"]
-  const [color, setColor] = useState(colors[6])
-  
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [quote, setQuote] = useState({ quote: "", author: "" });
+  const [getNewQuote, setGetNewQuote] = useState(0);
+  const tweetContent = '"' + quote.quote + '" -' + quote.author;
+  const colors = [
+    "#2c3e50",
+    "#342224",
+    "#16a085",
+    "#77b1a9",
+    "#e74c3c",
+    "#f39c12",
+    "#73a857",
+    "#bdbb99",
+    "#472e32",
+    "#27ae60",
+  ];
+  const [color, setColor] = useState(colors[6]);
+
   useEffect(() => {
-    let fetchWorking = true
+    let fetchWorking = true;
     if (fetchWorking) {
-      fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
-        .then(res => {
+      fetch(
+        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+      )
+        .then((res) => {
           if (res.ok) {
-            setError(false)
-            setLoading(false)
-            return res.json()
+            setError(false);
+            setLoading(false);
+            return res.json();
           } else {
-            setLoading(false)
-            throw Error("error")
+            setLoading(false);
+            throw Error("error");
           }
         })
-        .then(data => {
-          const randomIndex = Math.floor(Math.random() * data.quotes.length)
-          setQuote(data.quotes[randomIndex])
+        .then((data) => {
+          const randomIndex = Math.floor(Math.random() * data.quotes.length);
+          setQuote(data.quotes[randomIndex]);
         })
-        .catch(e => {
-          console.error(e)
-          setError(true)
-        })
+        .catch((e) => {
+          console.error(e);
+          setError(true);
+        });
     }
-      return () => fetchWorking = false
-  }, [getNewQuote])
+    return () => (fetchWorking = false);
+  }, [getNewQuote]);
 
   const handleClick = () => {
-    setGetNewQuote(prev => prev + 1)
-    const randomIndex = Math.floor(Math.random() * colors.length)
-    setColor(colors[randomIndex])
-  }
+    setGetNewQuote((prev) => prev + 1);
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    setColor(colors[randomIndex]);
+  };
 
-  if (loading) return <Loading />
-  if (error) return <Error />
-  return(
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+  return (
     <Container color={color}>
       <QuoteBox id="quote-box">
         <Quote id="text" color={color}>
-          <QuoteLeftIcon icon={faQuoteLeft} color={color}/>
+          <QuoteLeftIcon icon={faQuoteLeft} color={color} />
           {quote.quote}
         </Quote>
-        <Author id="author" color={color}>- {quote.author}</Author>
+        <Author id="author" color={color}>
+          - {quote.author}
+        </Author>
         <div>
           <div>
-            <IconBox id="tweet-quote" color={color} href={`https://twitter.com/intent/tweet?hashtags=quotes,freecodecamp&text=${tweetContent}`} target="_blank">
+            <IconBox
+              id="tweet-quote"
+              color={color}
+              href={`https://twitter.com/intent/tweet?hashtags=quotes,freecodecamp&text=${tweetContent}`}
+              target="_blank"
+            >
               <Icon icon={faTwitter} />
             </IconBox>
-            <IconBox id="tumblr-quote" color={color} href={`http://tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&content=${quote.quote}&&caption=${quote.author}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons`} target="_blank">
-              <Icon icon={faTumblr}/>
+            <IconBox
+              id="tumblr-quote"
+              color={color}
+              href={`http://tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&content=${quote.quote}&&caption=${quote.author}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons`}
+              target="_blank"
+            >
+              <Icon icon={faTumblr} />
             </IconBox>
           </div>
-          <Button id="new-quote" color={color} onClick={handleClick}>New Quote</Button>
+          <Button id="new-quote" color={color} onClick={handleClick}>
+            New Quote
+          </Button>
         </div>
       </QuoteBox>
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,24 +1,31 @@
-import styled from "styled-components"
-import { CommonContainer } from "../styles/styles"
+import styled from "styled-components";
+
+import { CommonContainer } from "../styles/styles";
 
 const Container = styled(CommonContainer)`
-    background-color: #e9a0a0;
-`
+  background-color: #e9a0a0;
+`;
 const ErrorGif = styled.img`
-    width: 150px;
-    @media (min-width: 600px) {
-        width: 300px;
-    }
-`
+  width: 150px;
+  @media (min-width: 600px) {
+    width: 300px;
+  }
+`;
 const ErrorMessage = styled.h1`
-    text-align: center;
-    letter-spacing: 2px;
-`
+  text-align: center;
+  letter-spacing: 2px;
+`;
 
-const Error = () => 
-    <Container>
-        <ErrorGif src="https://media.giphy.com/media/f0BaErqmljUd2/giphy.gif" alt="error gif" />
-        <ErrorMessage>Oops! Something went wrong. Please try again later.</ErrorMessage>
-    </Container>
+const Error = () => (
+  <Container>
+    <ErrorGif
+      src="https://media.giphy.com/media/f0BaErqmljUd2/giphy.gif"
+      alt="error gif"
+    />
+    <ErrorMessage>
+      Oops! Something went wrong. Please try again later.
+    </ErrorMessage>
+  </Container>
+);
 
-export default Error
+export default Error;
